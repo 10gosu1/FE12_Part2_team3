c68c635620be09bc1acb97b9e2aedfb19b1e190c
김승석 <kss@gimseungseog-ui-MacBookAir.local>
2024-12-15 22:03:57 +0900

    :sparkles:feat: headr 컴포넌트 생성 및 global.jsx 수정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,28 +1,17 @@
-import GlobalStyle from './../styles/global';
 import Title from './../components/Title';
 import InterestIdol from './../components/InterestIdol';
 import InterestIdolAdd from './../components/InterestIdolAdd';
-import styled from 'styled-components';
-
-const Inner = styled.div`
-  max-width: 1200px;
-  width: 100%;
-  margin: 0 auto;
-`;
 
 const Mypage = () => {
   // if (error) return <div>Error: {error.message}</div>;
 
   return (
     <>
-      <GlobalStyle />
       <section id="mypage">
-        <Inner>
-          <Title>내가 관심있는 아이돌</Title>
-          <InterestIdol />
-          <Title>관심 있는 아이돌을 추가해보세요.</Title>
-          <InterestIdolAdd />
-        </Inner>
+        <Title>내가 관심있는 아이돌</Title>
+        <InterestIdol />
+        <Title>관심 있는 아이돌을 추가해보세요.</Title>
+        <InterestIdolAdd />
       </section>
     </>
   );
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,19 +1,10 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+import bg from './../assets/header/bg.png';
 
 const GlobalStyle = createGlobalStyle`
-    /* Global Reset */
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
 
-    html, body {
-        background-color: var(--black-200);
-    }
-
-    /* Root Variables */
-    :root {
+/* Root Variables */
+:root {
         --black-200: #02000E;
         --black-100: #181D26;
         
@@ -31,6 +22,114 @@ const GlobalStyle = createGlobalStyle`
         --breakpoint-sm: 576px;
         --breakpoint-md: 768px;
         --breakpoint-lg: 1024px;
+    }
+    /* Global Reset */
+    html {}
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-size: 16px;
+        background: #fff;
+        color: var(--white);
+        background: url(${bg}) no-repeat 0% 0%;
+        background-color: var(--black-200);
+        padding-top: 80px;
+    }
+
+    body {
+        font-family: Pretendard Variable,Pretendard,-apple-system,BlinkMacSystemFont,system-ui,Roboto,Helvetica Neue,Segoe UI,Apple SD Gothic Neo,Noto Sans KR,Malgun Gothic,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,sans-serif
+    }
+
+    html,h1,h2,h3,h4,h5,h6,form,fieldset,img {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-weight: 400
+    }
+
+    h1,h2,h3,h4,h5,h6 {
+        font-size: 16px;
+        font-family: Pretendard Variable,Pretendard,-apple-system,BlinkMacSystemFont,system-ui,Roboto,Helvetica Neue,Segoe UI,Apple SD Gothic Neo,Noto Sans KR,Malgun Gothic,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,sans-serif
+    }
+
+    article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {
+        display: block
+    }
+
+    ul,dl,dt,dd {
+        margin: 0;
+        padding: 0;
+        list-style: none
+    }
+
+    legend {
+        position: absolute;
+        margin: 0;
+        padding: 0;
+        font-size: 0;
+        line-height: 0;
+        text-indent: -9999em;
+        overflow: hidden
+    }
+
+    label,input,button,select,img {
+        vertical-align: middle;
+        font-size: 16px;
+    }
+
+    input,button {
+        margin: 0;
+        padding: 0;
+        font-family: Pretendard Variable,Pretendard,-apple-system,BlinkMacSystemFont,system-ui,Roboto,Helvetica Neue,Segoe UI,Apple SD Gothic Neo,Noto Sans KR,Malgun Gothic,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,sans-serif;
+        font-size: 16px;
+    }
+
+    input[type=submit],button {
+        cursor: pointer
+    }
+
+    textarea,select {
+        font-family: Pretendard Variable,Pretendard,-apple-system,BlinkMacSystemFont,system-ui,Roboto,Helvetica Neue,Segoe UI,Apple SD Gothic Neo,Noto Sans KR,Malgun Gothic,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,sans-serif;
+        font-size: 16px;
+    }
+
+    select {
+        margin: 0
+    }
+
+    p {
+        margin: 0;
+        padding: 0;
+        word-break: break-all
+    }
+
+    hr {
+        display: none
+    }
+
+    pre {
+        overflow-x: scroll;
+        font-size: 1.6rem
+    }
+
+    a {
+        color: var(--white);
+        text-decoration: none
+    }
+
+    *,:after,:before {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box
+    }
+
+    input[type=text],input[type=password],textarea {
+        outline: none
+    }
+
+    input[type=text]:focus,input[type=password]:focus,textarea:focus,select:focus {
+        box-shadow: none
     }
 
     /* Button Styles */
@@ -51,6 +150,14 @@ const GlobalStyle = createGlobalStyle`
         &:focus {
             background: var(--gray-200);
         }
+    }
+
+    /* layout */
+
+    .inner {
+        max-width: 1200px;
+        width: 100%;
+        margin: 0 auto;
     }
 
     /* Media Queries for Responsive Design */
@@ -74,7 +181,7 @@ const GlobalStyle = createGlobalStyle`
             
         }
     }
+
 `;
 
 export default GlobalStyle;
-
