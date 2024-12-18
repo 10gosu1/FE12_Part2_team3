afeea2b4353decbe56c52734027002af2a0191d5
서경수 <kssuh2956@gmail.com>
2024-12-18 09:20:37 +0900

    ✨ Feat: IdolChart UI 개선
    차트 투표하기, 여자/남자아이돌 탭 추가, css추가

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Credit from '../components/Credit';
 import Waiting from '../components/DonationList';
 import Chart from '../components/Chart';
+import IdolChartPage from '../components/IdolChartPage';
 
 let StyleTest = styled.div`
   border: 1px solid red;
@@ -13,6 +14,7 @@ const List = () => {
       <Credit />
       <Waiting />
       <Chart />
+      <IdolChartPage />
     </StyleTest>
   );
 };
